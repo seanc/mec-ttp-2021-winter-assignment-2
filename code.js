08342e794e1b0fb3d78962b90e442d4ff503c669
Jon Chin <jon@sharebyteaching.org>
2021-01-05 03:17:25 -0500

    first draft

---
+++ code.js
@@ -0,0 +1,7 @@
+/*
+   MEC TTP 2021 Winter Assignment 2: Blackjack
+
+   Use this js file to write your project outline and your core game logic.
+   You will use your browser's javascript console to interact with the game.
+
+ */
