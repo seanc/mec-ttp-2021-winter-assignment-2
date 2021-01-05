35184a20a5091386f587bb7ffa68a357f04e70c2
Jon Chin <jon@sharebyteaching.org>
2021-01-05 03:21:18 -0500

    updated assignment name

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 /*
-   MEC TTP 2021 Winter Assignment 2: Blackjack
+   MEC TTP 2021 Winter Assignment 2: Blackjack Game Engine
 
    Use this js file to write your project outline and your core game logic.
    You will use your browser's javascript console to interact with the game.
