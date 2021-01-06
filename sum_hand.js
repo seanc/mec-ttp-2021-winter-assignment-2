54b13d364571e45a0f02a656cbee463caa5abef0
Athora190 <jademarte7@gmail.com>
2021-01-06 14:09:28 -0500

    more changes

--- sum_hand.js
+++ sum_hand.js
@@ -1,19 +1,12 @@
+// the sum of cards in the player's hand
 function sumHand (hand) {
-  
+    for(let i = 0; i < hand.length; i++)
+    {
+      let sum = hand[i] + hand[i+1]
+    }
 } 
 
 export default sumHand;
-
-// the sum of cards in the player's hand
-function sum_player_hand(hand)
-{
-  for(let i = 0; i < hand.length; i++)
-  {
-    let sum = hand[i] + hand[i+1]
-  }
-}
-export default sum_player_hand;
-
 
 // the sum of cards in the dealer's hand
 function sum_dealer_hand(hand)
